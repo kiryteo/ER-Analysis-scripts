e7b4f65cc8495f507c61eda65d9b9fcc97ad5e92
Ashwin Samudre <capnoi8@gmail.com>
2022-01-17 17:38:30 -0800

    beads properties steps script added

---
+++ beads_props_steps.m
@@ -0,0 +1,19 @@
+% MATLAB Steps for beads properties
+
+function props = beads_props_steps(filename)
+
+
+img = imread(filename);
+grimg = im2gray(img);
+thr = multithresh(grimg, 2);
+seg = imquantize(img, thr);
+op = label2rgb(seg);
+grop = im2gray(op);
+beads = (grop == max(grop));
+beads = double(beads);
+
+bw = bwconncomp(beads, 8);
+labeled = labelmatrix(bw);
+props = regionprops('table', labeled, 'Area', 'Circularity', 'ConvexArea', 'ConvexHull', 'Eccentricity', 'EulerNumber', 'MajorAxisLength', 'MaxFeretProperties', 'MinFeretProperties', 'MinorAxisLength', 'Orientation', 'Perimeter');
+
+% Further props are possible with passing grayscale image, image intensity specific props
