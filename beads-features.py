2e57405a325523b116f8ef6460670ad15957e1d5
Ashwin Samudre <capnoi8@gmail.com>
2022-02-03 17:40:28 -0800

    code refactor for beads-features script

--- beads-features.py
+++ beads-features.py
@@ -11,17 +11,13 @@ import glob
 import os
 import ks_multithresh
 import cv2
+import scipy.io
+
 
 home = os.path.expanduser('~')
 
 # path to data
 prefix = home + '/Documents/ER-Full-data/FixedCell_for_Ashwin/FixedCell_for_Ashwin/numpys/'
-
-# define global names for dir names
-# dirname = prefix + 'climp/'
-# climp_beads_dir = prefix + 'climp-beads/'
-# ctrl_beads_dir = prefix + 'control-beads/'
-# rtn_beads_dir = prefix + 'rtn-beads/'
 
 def get_beads(group, modality):
 
@@ -32,28 +28,42 @@ def get_beads(group, modality):
     beads_dir = prefix + group + '-beads/'
 
     for file in files:
-        np_data = np.load(file)
-        name = file.split('/')[-1].split('.')[0]
+        beads, fname = beads_multithresh(file, beads_dir)
 
-        if modality == 'confocal':
-            data = np_data[0]
-            fname = beads_dir + name + '_beads-conf.png'
-        elif modality == 'sted':
-            data = np_data[1]
-            fname = beads_dir + name + '_beads-sted.png'
-        elif modality == 'synth':
-            data = np_data[2]
-            fname = beads_dir + name + '_beads-synth.png'
+        get_beads_images(beads, fname)
+        matname = fname.split('.')[0] + '.mat'
 
-        beads = ks_multithresh.test_thresholds(data)
-        cv2.imwrite(fname, beads)
+        get_beads_mat(beads, matname)
+
+def beads_multithresh(file, beads_dir):
+    np_data = np.load(file)
+    name = file.split('/')[-1].split('.')[0]
+
+    if modality == 'confocal':
+        data = np_data[0]
+        fname = beads_dir + name + '_beads-conf.png'
+    elif modality == 'sted':
+        data = np_data[1]
+        fname = beads_dir + name + '_beads-sted.png'
+    elif modality == 'synth':
+        data = np_data[2]
+        fname = beads_dir + name + '_beads-synth.png'
+
+    beads = ks_multithresh.test_thresholds(data)
+    return beads, fname
+
+def get_beads_images(beads, fname):
+    cv2.imwrite(fname, beads)
+
+def get_beads_mat(beads, matname):
+    scipy.io.savemat(matname, {'npy': beads})
 
 # get_beads('climp', 'confocal')
-get_beads('climp', 'sted')
-get_beads('climp', 'synth')
-get_beads('control', 'confocal')
-get_beads('control', 'sted')
-get_beads('control', 'synth')
-get_beads('rtn', 'confocal')
-get_beads('rtn', 'sted')
-get_beads('rtn', 'synth')
+# get_beads('climp', 'sted')
+# get_beads('climp', 'synth')
+# get_beads('control', 'confocal')
+# get_beads('control', 'sted')
+# get_beads('control', 'synth')
+# get_beads('rtn', 'confocal')
+# get_beads('rtn', 'sted')
+# get_beads('rtn', 'synth')
