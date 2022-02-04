08372221f5a0ba7819962d3fc1b3b1fa11c9032d
Ashwin Samudre <capnoi8@gmail.com>
2022-02-03 16:44:54 -0800

    created get_beads module

--- beads-features.py
+++ beads-features.py
@@ -12,30 +12,48 @@ import os
 import ks_multithresh
 import cv2
 
-prefix = '/localhome/asa420/Documents/ER-Full-data/FixedCell_for_Ashwin/FixedCell_for_Ashwin/numpys/'
+home = os.path.expanduser('~')
 
-dirname = prefix + 'climp/'
-climp_beads_dir = prefix + 'climp-beads/'
-ctrl_beads_dir = prefix + 'control-beads/'
-rtn_beads_dir = prefix + 'rtn-beads/'
+# path to data
+prefix = home + '/Documents/ER-Full-data/FixedCell_for_Ashwin/FixedCell_for_Ashwin/numpys/'
 
-files = glob.glob(dirname + '*')
+# define global names for dir names
+# dirname = prefix + 'climp/'
+# climp_beads_dir = prefix + 'climp-beads/'
+# ctrl_beads_dir = prefix + 'control-beads/'
+# rtn_beads_dir = prefix + 'rtn-beads/'
 
-for file in files:
-    data = np.load(file)
-    conf = data[0]
-    sted = data[1]
-    synth = data[2]
+def get_beads(group, modality):
 
-    name = file.split('/')[-1].split('.')[0]
-    conf_name = name + '_beads-conf.png'
-    sted_name = name + '_beads-sted.png'
-    synth_name = name + '_beads-synth.png'
+    # path to npy files with images for all modalities
+    files = glob.glob(prefix + group + '/*')
 
-    conf_beads = ks_multithresh.test_thresholds(conf)
-    sted_beads = ks_multithresh.test_thresholds(sted)
-    synth_beads = ks_multithresh.test_thresholds(synth)
+    # path to output dir specific to the group
+    beads_dir = prefix + group + '-beads/'
 
-    cv2.imwrite(conf_name, conf_beads)
-    cv2.imwrite(sted_name, sted_beads)
-    cv2.imwrite(synth_name, synth_beads)
+    for file in files:
+        np_data = np.load(file)
+        name = file.split('/')[-1].split('.')[0]
+
+        if modality == 'confocal':
+            data = np_data[0]
+            fname = beads_dir + name + '_beads-conf.png'
+        elif modality == 'sted':
+            data = np_data[1]
+            fname = beads_dir + name + '_beads-sted.png'
+        elif modality == 'synth':
+            data = np_data[2]
+            fname = beads_dir + name + '_beads-synth.png'
+
+        beads = ks_multithresh.test_thresholds(data)
+        cv2.imwrite(fname, beads)
+
+# get_beads('climp', 'confocal')
+get_beads('climp', 'sted')
+get_beads('climp', 'synth')
+get_beads('control', 'confocal')
+get_beads('control', 'sted')
+get_beads('control', 'synth')
+get_beads('rtn', 'confocal')
+get_beads('rtn', 'sted')
+get_beads('rtn', 'synth')
