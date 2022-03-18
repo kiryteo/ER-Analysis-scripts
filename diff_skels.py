74b02ad70855f0fcd072ca0d4efeb5e52c3ae4ca
Ashwin Samudre <capnoi8@gmail.com>
2022-03-17 17:11:30 -0700

    frequency filters for improved network extraction

--- diff_skels.py
+++ diff_skels.py
@@ -17,7 +17,6 @@ from sklearn.metrics import jaccard_score
 stdimg = io.imread('/localhome/asa420/MIAL/data/live-cell-movies/COSKDEL/COSKDEL/Decon/Series002_decon_converted/std/Series002_decon_converted_t65_ch00_std.png')
 img = io.imread('/localhome/asa420/MIAL/data/live-cell-movies/COSKDEL/COSKDEL/Decon/Series002_decon_converted/enh/Series002_decon_converted_t65_ch00_std_enhance.png')
 
-
 # img0 =
 
 def get_image(img):
@@ -90,61 +89,91 @@ def skimage_dilate_img(img):
 def skimage_erode_img(img):
     return skimage.morphology.erosion(img.astype('int'))
 
-er1 = skimage_erode_img(stdimg)
-loc = thresh_loc(er1)
-er2 = skimage_erode_img(loc)
-er3 = skimage_erode_img(er2)
 
-mh_thin = mahotas_thin(er3)
-skcv = cv_skel(er3.astype('uint8'), 'e')
+def plot_samples():
+    fig, ax = plt.subplots(1, 2)
 
-biner3 = threshold_niblack(er3)
-skghcv = cv_skel(biner3.astype('uint8'), 'gh')
-skpcv = pcv_skel(biner3)
-sksk = skimg_skel(biner3.astype('int')/255)
-skth = skimg_thin(er3.astype('int')/255)
-skmed = skimg_medaxis(er3.astype('int')/255)
+    ax[0].imshow(dilsk)
+    ax[1].imshow(dilavg)
 
-#biner2 = (er2>0)
-#print(np.unique(er2))
-#loc2 = thresh_loc(er3, 3, 'median', 0, 'nearest')
+    plt.show()
 
-# print(np.unique(loc2))
+    # fig, ax = plt.subplots(2, 6)
+    #
+    # ax[0][0].imshow(stdimg)
+    # # ax[0][1].imshow(er1)
+    # # ax[0][2].imshow(loc)
+    # # ax[0][3].imshow(er2)
+    # ax[0][4].imshow(er3)
+    # ax[0][5].imshow(mh_thin)
+    # ax[1][0].imshow(skcv)
+    # ax[1][1].imshow(skghcv)
+    # ax[1][2].imshow(skpcv)
+    # ax[1][3].imshow(sksk)
+    # ax[1][4].imshow(skth)
+    # ax[1][5].imshow(skmed)
+    # plt.show()
 
-fig, ax = plt.subplots(2, 6)
+    # ax[0].imshow(dipimg)
+    # ax[1].imshow(thrlocal3)
+    # ax[2].imshow(thrlocal5)
+    # ax[3].imshow(thrlocal7)
+    #
+    # # ax[0].imshow(skel_dip_three)
+    # # # ax[1].imshow(thin)
+    # # # ax[2].imshow(medax)
+    # # # ax[3].imshow(skel_pcv)
+    # # # ax[4].imshow(skel_cv)
+    # # # ax[5].imshow(skel_cv_gh)
+    # # # ax[6].imshow(skel_maho)
+    # # ax[1].imshow(skel_dip_two)
+    # # ax[2].imshow(skel_dip_one)
+    # # ax[3].imshow(skel_dip_nat)
+    #
+    # plt.show()
 
-ax[0][0].imshow(stdimg)
-# ax[0][1].imshow(er1)
-# ax[0][2].imshow(loc)
-# ax[0][3].imshow(er2)
-ax[0][4].imshow(er3)
-ax[0][5].imshow(mh_thin)
-ax[1][0].imshow(skcv)
-ax[1][1].imshow(skghcv)
-ax[1][2].imshow(skpcv)
-ax[1][3].imshow(sksk)
-ax[1][4].imshow(skth)
-ax[1][5].imshow(skmed)
-plt.show()
+stdimg = io.imread('/localhome/asa420/MIAL/data/live-cell-movies/COSKDEL/COSKDEL/Decon/Series002_decon_converted/std/Series002_decon_converted_t00_ch00_std.png')
+
+def exp():
+    er1 = skimage_erode_img(stdimg)
+    loc = thresh_loc(er1)
+    er2 = skimage_erode_img(loc)
+    er3 = skimage_erode_img(er2)
+
+    mh_thin = mahotas_thin(er3)
+    skcv = cv_skel(er3.astype('uint8'), 'e')
+
+    # fig, ax = plt.subplots(1, 4)
+    # ax[0].imshow(er1)
+    # ax[1].imshow(loc)
+    # ax[2].imshow(er2)
+    # ax[3].imshow(er3)
+
+    # biner3 = threshold_niblack(er3)
+    # skghcv = cv_skel(biner3.astype('uint8'), 'gh')
+    # skpcv = pcv_skel(biner3)
+    # sksk = skimg_skel(biner3.astype('int')/255)
+    # skth = skimg_thin(er3.astype('int')/255)
+    # skmed = skimg_medaxis(er3.astype('int')/255)
+
+    # fsk = io.imread('/localhome/asa420/MIAL/data/live-cell-movies/COSKDELCLIMP/COSKDELCLIMP/Decon/Series007_decon_converted/skel/Series007_decon_converted_t00_ch00_std_enhance_skel.png')
+    # favg = io.imread('/localhome/asa420/MIAL/data/aggregation-with-median/Climp/avg/skel/ClimpSeries7-avg_skel.png')
+    #
+    # dilsk = skimage_dilate_img(fsk)
+    # dilavg = skimage_dilate_img(favg)
+
+
+
+
+    # timg = io.imread('/localhome/asa420/ER-Analysis-scripts/ero5.png')
+    #
+    op = skimage.filters.butterworth(er3, cutoff_frequency_ratio=0.02, order=3.0)#, high_pass=False)
+    # k = np.where(op < 0)
+    # op[k]=0
+    plt.imshow(op)
+    plt.colorbar()
+    plt.show()
+
+exp()
 
 # skel_dip_away = dip.EuclideanSkeleton(dipimg, endPixelCondition='loose ends away') ####### not useful
-
-# fig, ax =
-#
-# ax[0].imshow(dipimg)
-# ax[1].imshow(thrlocal3)
-# ax[2].imshow(thrlocal5)
-# ax[3].imshow(thrlocal7)
-#
-# # ax[0].imshow(skel_dip_three)
-# # # ax[1].imshow(thin)
-# # # ax[2].imshow(medax)
-# # # ax[3].imshow(skel_pcv)
-# # # ax[4].imshow(skel_cv)
-# # # ax[5].imshow(skel_cv_gh)
-# # # ax[6].imshow(skel_maho)
-# # ax[1].imshow(skel_dip_two)
-# # ax[2].imshow(skel_dip_one)
-# # ax[3].imshow(skel_dip_nat)
-#
-# plt.show()
