957acb63aeacdf31633ce7405dddfdd6ba32f5fe
Ashwin Samudre <capnoi8@gmail.com>
2022-03-21 16:17:35 -0700

    Vesselization matlab scripts added

---
+++ vess_runer.m
@@ -0,0 +1,7 @@
+for i=11:11
+dirname = ['/localhome/asa420/MIAL/data/live-cell-movies/COSKDELRTN/COSKDELRTN/Decon/Series0' int2str(i) '_decon_converted/erode/'];
+for j=13:99
+fname = [dirname 'Series0' int2str(i) '_decon_converted_t' int2str(j) '_ch00_std_erode.png'];
+Vessel2d(fname);
+end;
+end;
