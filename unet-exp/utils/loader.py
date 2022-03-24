b4b3ab48bdd8232a3e516d1b474089f481c9a8b5
Ashwin Samudre <capnoi8@gmail.com>
2022-03-23 21:51:46 -0700

    dataloader updated

--- unet-exp/utils/loader.py
+++ unet-exp/utils/loader.py
@@ -20,7 +20,7 @@ from torch.utils.data import Dataset
 home = os.path.expanduser('~')
 
 class Skeldataset(Dataset):
-    def __init__(self, imgdir):
+    def __init__(self, imgdir, transform=None):
         self.imgdir = Path(imgdir)
         # self.pgtdir = Path(pgtdir)
         # self.mpdir = Path(mpdir)
@@ -33,18 +33,18 @@ class Skeldataset(Dataset):
 
     def __getitem__(self, idx):
         name = self.ids[idx]
-        # img_file = Image.open(name)
-        img_file = io.imread(name)
+        img_file = Image.open(name)
+        # img_file = io.imread(name)
         pgt_file_name = name.split('/')
         pgt_file = home + '/unet-exp/data/PGT/' + pgt_file_name[6] + '/' + pgt_file_name[7].split('.')[0] + '_erode_enhance_projection.png'
         # pgt_file = pgt_file_name[0] + '/PGT/' + pgt_file_name[2] + '/' + pgt_file_name[3].split('.')[0] + '_erode_enhance_projection.png'
-        # pgt_file = Image.open(pgt_file)
-        pgt_file = io.imread(pgt_file)
+        pgt_file = Image.open(pgt_file)
+        # pgt_file = io.imread(pgt_file)
         sernum = int(name.split('/')[-1].split('_')[0][-3:])
         # mplabel = name.split('/')[0] + '/MPSkel/' + name.split('/')[2] + '/' + str(sernum) + '.png'
         mplabel = home + '/unet-exp/data/MPSkel/' + name.split('/')[6] + '/' + str(sernum) + '.png'
-        # mplabel = Image.open(mplabel)
-        mplabel = io.imread(mplabel)
+        mplabel = Image.open(mplabel)
+        # mplabel = io.imread(mplabel)
         return img_file, pgt_file, mplabel
 
 # imgdir = '/localhome/asa420/unet-exp/data/images'
@@ -54,19 +54,30 @@ class Skeldataset(Dataset):
 
 # train_dataloader = DataLoader(Skeldataset, batch_size=32, shuffle=False)
 
-skdata = Skeldataset(imgdir='/localhome/asa420/unet-exp/data/images')
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     ]
+)
 
-import matplotlib.pyplot as plt
-#for i in range(len(skdata)):
-for i in range(5):
-    sample = skdata[i]
-    # print(sample)
-    fig, ax = plt.subplots(1, 3)
-    ax[0].imshow(sample[0])
-    ax[1].imshow(sample[1])
-    ax[2].imshow(sample[2])
-    plt.show()
-    # sample[2].show()
-    #print(i, sample)
+skdata = Skeldataset(imgdir='/localhome/asa420/unet-exp/data/images', transform=transform)
 
-    # plt.imshow()
+
+# import matplotlib.pyplot as plt
+# #for i in range(len(skdata)):
+# for i in range(5):
+#     sample = skdata[i]
+#     # print(sample)
+#     fig, ax = plt.subplots(1, 3)
+#     ax[0].imshow(sample[0])
+#     ax[1].imshow(sample[1])
+#     ax[2].imshow(sample[2])
+#     plt.show()
+#     # sample[2].show()
+#     #print(i, sample)
+#
+#     # plt.imshow()
+tloader = DataLoader(skdata, batch_size=32, shuffle=True)
+
+dataiter = iter(tloader)
+im, pg, mp = dataiter.next()
+
