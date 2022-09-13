7cc66963b3e8119afe8bc81007e69c7f2a9d62b9
Ashwin Samudre <capnoi8@gmail.com>
2022-09-12 21:22:46 -0700

    Dilated tubule analysis and variance metric code done

--- tubular_analysis.py
+++ tubular_analysis.py
@@ -10,45 +10,8 @@ import networkx as nx
 import numpy as np
 import cv2
 
-# mean_skel = imageio.imread('/localhome/asa420/MIAL/data/confocal_movies/ATL/new_op_jul/ATL_mean_proj/A1_mean.png')
-#
-# svpath = '/localhome/asa420/MIAL/data/confocal_movies/ATL/new_op_jul/new_process_frames/'
-#
-# for i in range(100):
-#     er = cv2.imread('/localhome/asa420/MIAL/data/confocal_movies/ATL/files/A1_decon_t0%s_ch00.png'%f'{i:02d}')
-#     skel = imageio.imread('/localhome/asa420/MIAL/data/confocal_movies/ATL/new_op_jul/skel/A1/A1_decon_t0%s_ch00_skel.png'%f'{i:02d}')
-#     er_skel = cv2.imread('/localhome/asa420/MIAL/data/confocal_movies/ATL/new_op_jul/er_skel_overlay/A1_decon_t0%s_ch00_er_overlay.png'%f'{i:02d}')
-#
-#     fig = plt.figure(figsize=(4, 2))
-#     #plt.title('A%s_decon_t0%s_ch00_frame'%(f'{i}', f'{j:02d}'))
-#     plt.axis('off')
-#     r, c = 1, 2
-#
-#     fig.add_subplot(r, c, 1)
-#     plt.imshow(er)
-#     plt.axis('off')
-#     plt.title('Input')
-#
-#     # fig.add_subplot(r, c, 2)
-#     # plt.imshow(skel)
-#     # plt.axis('off')
-#     # plt.title('Skeleton')
-#     #
-#     # fig.add_subplot(r, c, 3)
-#     # plt.imshow(mean_skel)
-#     # plt.axis('off')
-#     # plt.title('Mean projection skel')
-#
-#     fig.add_subplot(r, c, 2)
-#     plt.imshow(er_skel)
-#     plt.axis('off')
-#     plt.title('Overlay')
-#
-#     plt.savefig(svpath + 'A1_decon_t0%s_ch00_new_frame_two.png'%(f'{i:02d}'), bbox_inches='tight', pad_inches=0.1)
-#     plt.close()
-#
-#
-# exit()
+
+
 
 # from skan import draw
 #
@@ -65,62 +28,17 @@ import cv2
 # exit()
 
 
-# mean_proj_img = imageio.imread('/localhome/asa420/MIAL/data/confocal_movies/ATL/new_op_jul/ATL_mean_proj/A1_mean.png')
-# # plt.imshow(mean_proj_img)
-#
-# # bin_img = mean_proj_img > 0
-# thresh = threshold_otsu(mean_proj_img)
-# bin_img = mean_proj_img > thresh
-#
-# skel = pcv.morphology.skeletonize(mask=bin_img)
-# g = sknw.build_sknw(skel, iso=False)
-# G = nx.Graph()
-#
-# G.add_nodes_from(g.nodes)
-# G.add_edges_from(g.edges)
-#
-# nodes = g.nodes()
-# ps = np.array([nodes[i]['o'] for i in nodes])
-#
-# # nx.draw_networkx(G, pos=pos, with_labels=True, node_size=10)
-# degree_list = G.degree
-#
-# # get all the nodes with degree greater than 2
-# newps = []
-# for i, val in enumerate(degree_list):
-#     if val[1] > 2:
-#         newps.append(ps[i])
-#
-# brpts_img = np.zeros((128, 128))
-# # brpts_img[newps] = 1.
-# for each in newps:
-#     brpts_img[each[0], each[1]] = 255.
-#
-# dil_brpts = pcv.dilate(gray_img=brpts_img, ksize=3, i=1)
-# tubules = skel - dil_brpts
-#
-# tubules[np.where(tubules<0)] = 0
-#
-# lab_tubules = skimage.measure.label(tubules)
-#
-# plt.imshow(lab_tubules)
-# plt.show()
-# pcv.print_image(lab_tubules, '/localhome/asa420/MIAL/data/confocal_movies/ATL/new_op_jul/A1_mean_skel_lab_tubules.png')
-# cv2.imwrite('/localhome/asa420/MIAL/data/confocal_movies/ATL/new_op_jul/A1_mean_skel_lab_tubules.png', lab_tubules)
-
-
-
 def tub_analysis():
-    mean_proj_img = imageio.imread('/localhome/asa420/MIAL/data/confocal_movies/ATL/new_op_jul/ATL_mean_proj/A1_mean.png')
-# plt.imshow(mean_proj_img)
 
-# bin_img = mean_proj_img > 0
+    mean_proj_img = imageio.imread('/localhome/asa420/MIAL/data/confocal_movies/ATL/new_op_jul/ATL_mean_proj/A1_mean.png')
+
+    # perform thresholding + binarization + skel
     thresh = threshold_otsu(mean_proj_img)
     bin_img = mean_proj_img > thresh
 
     skel = pcv.morphology.skeletonize(mask=bin_img)
-    # branchpts = pcv.morphology.find_branch_pts(skel_img=skel)
 
+    # Build graph from the skeleton
     g = sknw.build_sknw(skel, iso=False)
     G = nx.Graph()
 
@@ -139,18 +57,19 @@ def tub_analysis():
         if val[1] > 2:
             newps.append(ps[i])
 
+    # Obtain the junctions
     brpts_img = np.zeros((128, 128))
     # brpts_img[newps] = 1.
     for each in newps:
         brpts_img[each[0], each[1]] = 255.
 
+    # Dilate junctions and remove them to get individual tubules
     dil_brpts = pcv.dilate(gray_img=brpts_img, ksize=3, i=1)
     tubules = skel - dil_brpts
 
     tubules[np.where(tubules<0)] = 0
 
-
-
+    # label tubules
     lab_tubules = skimage.measure.label(tubules)
 
     return lab_tubules
@@ -164,94 +83,108 @@ def tub_analysis():
 
 lab_tubules = tub_analysis()
 
-dil_lab_tubules = skimage.morphology.dilation(lab_tubules)
 
 # print(np.where(lab_tubules==102))
 # print(np.where(dil_lab_tubules==102))
 # plt.imshow(lab_tubules)
 # plt.show()
 
-# X, Y = np.where(lab_tubules==84)
+
+def get_tubule_XY(lab_tubules):
+    lt = {}
+    # lt = []
+    for each in range(1, len(np.unique(lab_tubules))):
+        tub_len = len(np.where(lab_tubules==each)[0])
+        if tub_len > 5:
+            # lt.append(np.where(lab_tubules==each))
+            lt[each] = np.where(lab_tubules==each)
+
+    coords = {}
+    for num, pts in lt.items():
+        coords[num] = []
+        for (x, y) in zip(pts[0], pts[1]):
+            coords[num].append((x,y))
+    return coords
+
+
+
+# lt = []
+# lt.append(np.where(lab_tubules==102))
 #
-# dil_intensity_dict = {}
-# dil_int_list = []
-# for i, j in zip(X, Y):
-#     dil_int_list.append(lab_tubules[i,j])
-#     dil_intensity_dict[(i, j)] = ((lab_tubules[i,j] + lab_tubules[i+1,j] + lab_tubules[i-1,j] + lab_tubules[i, j+1] + lab_tubules[i, j-1]) / 5)
 #
+# coords = {}
+# for num, pts in lt.items():
+#     coords[num] = []
+#     for (x, y) in zip(pts[0], pts[1]):
+#         coords[num].append((x,y))
 #
-# print(dil_int_list)
-# print(dil_intensity_dict)
-#
-# exit()
+# print(coords)
 
 
+coords = get_tubule_XY(lab_tubules)
 
 
-lt = {}
-for each in range(1, len(np.unique(lab_tubules))):
-    tub_len = len(np.where(lab_tubules==each)[0])
-    if tub_len > 5:
-        # lt.append(np.where(lab_tubules==each))
-        lt[each] = np.where(lab_tubules==each)
-
-
-# lt = {}
-# lt[102] = (np.where(lab_tubules==102))
-
-
-coords = {}
-for num, pts in lt.items():
-    coords[num] = []
-    for (x, y) in zip(pts[0], pts[1]):
-        coords[num].append((x,y))
-
-
-
-intensity_vals = []
-for i in range(100):
-    l = []
-    img = imageio.imread('/localhome/asa420/MIAL/data/confocal_movies/ATL/files/A1_decon_t0%s_ch00.tif'%f'{i:02d}')
-    img = (img - img.min()) / (img.max() - img.min())
-    for num, points in coords.items():
-        for (x, y) in points:
+ndt = {}
+for tubule_num, coord_list in coords.items():
+    # print(coord_list)
+    ndt[tubule_num] = []
+    for i in range(100):
+        img = imageio.imread('/localhome/asa420/MIAL/data/confocal_movies/ATL/files/A1_decon_t0%s_ch00.tif'%f'{i:02d}')
+        img = (img - img.min()) / (img.max() - img.min())
+        l = []
+        for (x, y) in coord_list:
             if (x > 0 and x < 127) and (y > 0 and y < 127):
-                l.append((img[x,y] + img[x+1,y] + img[x-1,y] + img[x,y-1] + img[x,y+1]) / 5)
+                mean_val = (img[x,y] + img[x+1,y] + img[x-1,y] + img[x,y-1] + img[x,y+1]) / 5
             else:
-                l.append(img[x,y])
-    intensity_vals.append(l)
+                mean_val = img[x,y]
+            l.append(mean_val)
+        ndt[tubule_num].append(l)
 
-# print(intensity_vals)
-# print(len(intensity_vals))
-# # exit()
-#
-# for tl in intensity_vals:
-#     print(len(tl))
-
-
-newlt = []
-
-for tub_list in range(len(intensity_vals[0])):
-    for frame in range(100):
-        newlt.append(intensity_vals[frame][tub_list])
-
-
+# print(len(ndt[1]))
+# print(len(ndt[1][4]))
 
 mean_list = []
-for i in range(0, len(newlt), 100):
-    mean_list.append(np.mean(newlt[i:i+100], axis=0))
+
+for k, v in ndt.items():
+    mean_list.append(np.mean(v, axis=0))
 
 
 
-print(mean_list[5])
-exit()
+
+# intensity_vals = []
+# for i in range(100):
+#     l = []
+#     img = imageio.imread('/localhome/asa420/MIAL/data/confocal_movies/ATL/files/A1_decon_t0%s_ch00.tif'%f'{i:02d}')
+#     img = (img - img.min()) / (img.max() - img.min())
+#     # for num, points in coords.items():
+#     #     for (x, y) in points:
+#
+#     for each in lt:
+#         l.append(img[each])
+#     intensity_vals.append(l)
+#
+
+# newlt = []
+#
+# # for tub_list in range(len(intensity_vals[0])):
+# #     for frame in range(100):
+# #         newlt.append(intensity_vals[frame][tub_list])
+#
+#
+# mean_list = []
+# for i in range(0, len(newlt), 100):
+#     mean_list.append(np.mean(newlt[i:i+100], axis=0))
+#
+# print(mean_list[5])
+# exit()
 
 for i, val in enumerate(mean_list):
-    plt.plot(mean_list[i])
-    plt.title('Intensity profile for highlighted tubule', fontsize=24)
-    plt.xlabel('Distance along tubule (pixels)', fontsize=20)
-    plt.ylabel('Average normalized intensity over time (100 frames)', fontsize=20)
-    plt.show()
+    # plt.plot(mean_list[i])
+    # plt.title('Intensity profile for highlighted tubule', fontsize=24)
+    # plt.xlabel('Distance along tubule (pixels)', fontsize=20)
+    # plt.ylabel('Average normalized intensity over time (100 frames)', fontsize=20)
+    # plt.show()
+    print(np.var(mean_list[i]))
 
 
 exit()
@@ -321,49 +254,3 @@ plt.suptitle('Junction detection')
 plt.title('Yellow: all branching areas, Red: Only degree 3 or more areas')
 plt.savefig('Refined_junction_detection', bbox_inches='tight')
 plt.close()
-
-
-
-# print(newps[0])
-# print(len(newps))
-
-# print(len(degree_list))
-#
-# print(degree_list)
-
-# pos = graphviz_layout(G)
-#
-# print(pos)
-
-# plt.show()
-# print(pos)
-
-# exit()
-
-# plt.imshow(tubules)
-# plt.imshow(lab_tubules)
-# plt.show()
-
-# plt.imshow(skel)
-# plt.imshow(tubules)
-
-# fig = plt.figure()
-# plt.axis('off')
-# r, c = 1, 4
-#
-# fig.add_subplot(r, c, 1)
-# plt.imshow(mean_proj_img)
-#
-# fig.add_subplot(r, c, 2)
-# plt.imshow(skel)
-#
-# fig.add_subplot(r, c, 3)
-# plt.imshow(tubules)
-#
-# fig.add_subplot(r, c, 4)
-# plt.imshow(lab_tubules)
-#
-# plt.show()
-
-# plt.imshow(bin_img)
-# plt.show()
