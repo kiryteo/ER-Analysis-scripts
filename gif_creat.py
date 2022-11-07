aeac32faeb6ac74c779687d16cf8989c6dbcd78d
Ashwin Samudre <capnoi8@gmail.com>
2022-11-06 18:44:14 -0800

    Gif creator file added

---
+++ gif_creat.py
@@ -0,0 +1,15 @@
+import glob
+import imageio
+
+# pref = '/localhome/asa420/MIAL/data/confocal_movies/RTN/new_op_jul/rtn_junc_viz/'
+
+
+pref = '/localhome/asa420/MIAL/data/confocal_movies/ATL/new_op_jul/junc_types_movies/'
+
+with imageio.get_writer('A1_junc_iso_fuz.gif', mode='I', duration=0.5) as writer:
+# with imageio.get_writer('A1_junc.mp4', fps=2) as writer:
+    for i in range(100):
+        filename = pref + 'A1_decon_t0%s_ch00.png'%f'{i:02d}'
+        image = imageio.imread(filename)
+        image = (image - image.min()) / (image.max() - image.min())
+        writer.append_data(image)
