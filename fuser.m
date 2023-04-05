89488abf76db188dc872f8ff0792d7a21789033f
kiryteo <ashwin.samudre@gmail.com>
2023-04-05 14:53:29 -0700

    Removed data files from the repo and added .gitignore file to ignore them in the future.

--- fuser.m
+++
@@ -1,8 +0,0 @@
-
-fname = ['/localhome/asa420/MIAL/data/CROP-n/Plos_data_crops/RTN_crops/Series006_decon_ch02-3'];
-tub = imread([fname '_tubules.png']);
-
-brpts = imread([fname '_dilbr.png']);
-
-f = imfuse(tub, brpts);
-imwrite(f, [fname '_skel_br.png']);
