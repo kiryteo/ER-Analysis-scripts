e6569398951fd40954192831ad56d92845b2ee8f
kiryteo <ashwin.samudre@gmail.com>
2023-04-12 23:44:21 -0700

    Ref_junc_analysis code improvement

--- src/nw_graph_analysis/ref_junc_variation_analysis.py
+++ src/nw_graph_analysis/ref_junc_variation_analysis.py
@@ -5,11 +5,56 @@ import pickle as pkl
 import seaborn as sns
 import statannot
 import matplotlib.pyplot as plt
+import itertools
 from junction_analysis_modules import JunctionAnalysis as JA
 
 confocal_data_path = '/localhome/asa420/MIAL/data/confocal_movies/'
 
 junc_analysis = JA(confocal_data_path)
+
+
+def get_group_box_pairs(channel):
+    groups = (
+        ['ATL', 'Climp', 'RTN']
+        if channel == 'mch'
+        else ['ATL', 'Climp', 'RTN', 'Control']
+    )
+    return list(itertools.combinations(groups, 2))
+
+
+def get_box_pairs(channel):
+    # Get group names
+    if channel == 'mch':
+        groups = ['ATL', 'Climp', 'RTN']
+    else:
+        groups = ['ATL', 'Climp', 'RTN', 'Control']
+
+    # Get region names
+    regions = ['R1', 'R2', 'R3']
+
+    # Get all pairs of groups across regions
+    box_pairs = []
+    for r in regions:
+        for i, m1 in enumerate(groups):
+            for m2 in groups[i+1:]:
+                pair = ((r, m1), (r, m2))
+                box_pairs.append(pair)
+
+    return box_pairs
+
+
+def get_per_frame_ref_junc_data(region_data, group_pref, dirname, channel, group, num):
+    data = []
+    for ref_junc in region_data:
+        # store 100 values per ref junc
+        junc_signal_vals = []
+
+        for i in range(100):
+            input_data = imageio.imread(f'{confocal_data_path}/{group}/new_op_jul/{dirname}/{group_pref[group]}{num}_decon_t0{i:02d}_ch{channel:02d}_std.png')
+            junc_signal_vals.append(input_data[ref_junc[0], ref_junc[1]])
+
+        data.append(junc_signal_vals)
+    return data
 
 
 def ref_junc_data_per_group(group, num_series, channel, region):
@@ -21,7 +66,6 @@ def ref_junc_data_per_group(group, num_series, channel, region):
     @return: group_data (list) - provides all reference junctions per movie with 100 values in a group
     """
     group_data = []
-    # grp_data_egfp, grp_data_mch = [], []
 
     dirname, ch = ('std_egfp', 0) if channel == 'egfp' else ('std_mch', 1)
     group_pref = {'ATL': 'A', 'Climp': 'C', 'Control': 'Ct', 'RTN': 'R'}
@@ -30,24 +74,14 @@ def ref_junc_data_per_group(group, num_series, channel, region):
         ref_junctions, per_frame_junctions, labelled_img = junc_analysis.label_junctions(group, num)
         label_ids, unassigned_cc_dict = junc_analysis.separate_junc_cc(ref_junctions, per_frame_junctions, labelled_img)
         iso, fuz, unk = junc_analysis.get_junction_areas(label_ids, unassigned_cc_dict)
+
         if region == 'iso':
             region_data = iso
         elif region == 'fuz':
             region_data = fuz
 
-        # store per movie list of ref junc list with 100 values
-        data = []
-        for CC_ref in region_data:
-            # store 100 values per ref junc
-            junc_signal_vals = []
+        data = get_per_frame_ref_junc_data(region_data, group_pref, dirname, ch, group, num)
 
-            for i in range(100):
-                input_data = imageio.imread(f'{confocal_data_path}/{group}/new_op_jul/{dirname}/{group_pref[group]}{num}_decon_t0{i:02d}_ch{ch:02d}_std.png')
-                junc_signal_vals.append(input_data[CC_ref[0], CC_ref[1]])
-
-            data.append(junc_signal_vals)
-
-        # store list of per movie data list
         group_data.append(data)
 
     return group_data
@@ -109,10 +143,7 @@ def plot_ref_junc_variation(channel, region, measure, plot_type):
         ax = sns.boxenplot(data=df, x='Group', y='data_tubule_mean')
         ax.set_xticklabels(ax.get_xticklabels(), fontsize=16)
 
-        if channel == 'egfp':
-            box_pairs = [('ATL', 'Climp'), ('ATL', 'RTN'), ('Climp', 'RTN'), ('ATL', 'Control'), ('Climp', 'Control'), ('RTN', 'Control')]
-        else:
-            box_pairs = [('ATL', 'Climp'), ('ATL', 'RTN'), ('Climp', 'RTN')]
+        box_pairs = get_group_box_pairs(channel)
 
         statannot.add_stat_annotation(ax, x='Group', y='data_tubule_mean', data=df, box_pairs=box_pairs,
                                     test='Mann-Whitney', text_format='simple', loc='inside', verbose=2, fontsize='large')
@@ -128,10 +159,7 @@ def plot_ref_junc_variation(channel, region, measure, plot_type):
         if channel == 'egfp':
             df['data_tubule_mean'] = pd.Series(np.concatenate((a1, a2, a3, c1, c2, c3, r1, r2, r3, ct1, ct2, ct3)))
 
-            df['Replicate'] = pd.Series(np.concatenate((['R1'] * len(a1), ['R2'] * len(a2), ['R3'] * len(a3), ['R1'] * len(c1),
-                                                    ['R2'] * len(c2), ['R3'] * len(c3), ['R1'] * len(r1), ['R2'] * len(r2),
-                                                    ['R3'] * len(r3), ['R1'] * len(ct1), ['R2'] * len(ct2),
-                                                    ['R3'] * len(ct3))))
+            df['Replicate'] = pd.Series(np.concatenate((['R1'] * len(a1), ['R2'] * len(a2), ['R3'] * len(a3), ['R1'] * len(c1), ['R2'] * len(c2), ['R3'] * len(c3), ['R1'] * len(r1), ['R2'] * len(r2), ['R3'] * len(r3), ['R1'] * len(ct1), ['R2'] * len(ct2), ['R3'] * len(ct3))))
             
             df['Group'] = pd.Series(np.concatenate((
             ['ATL'] * len(a1), ['ATL'] * len(a2), ['ATL'] * len(a3), ['Climp'] * len(c1), ['Climp'] * len(c2),
@@ -153,12 +181,7 @@ def plot_ref_junc_variation(channel, region, measure, plot_type):
 
         replicates = ['R1', 'R2', 'R3']
         
-        if channel == 'egfp':
-            groups = ['ATL', 'Climp', 'RTN', 'Control']
-        else:
-            groups = ['ATL', 'Climp', 'RTN']
-
-        box_pairs = [((x, y), (x, z)) for i, x in enumerate(replicates) for j, y in enumerate(groups) for z in groups[j + 1 :]]
+        box_pairs = get_box_pairs(channel, replicates)
 
         statannot.add_stat_annotation(ax, x='Replicate', y='data_tubule_mean', hue='Group', data=df, box_pairs=box_pairs,
                                       test='Mann-Whitney', text_format='simple', loc='inside', verbose=2, fontsize='large')
