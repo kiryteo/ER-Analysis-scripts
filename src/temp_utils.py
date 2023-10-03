0afbc9bba0c1843ce0f57de0dd1f5db0ef392edb
kiryteo <ashwin.samudre@gmail.com>
2023-10-03 00:25:31 -0700

    moved edt evolution to temp_utils

--- src/temp_utils.py
+++ src/temp_utils.py
@@ -17,6 +17,60 @@ confocal_data_path = '/localhome/asa420/MIAL/data/confocal_movies/'
 skel_path = '/localhome/asa420/MIAL/data/confocal_movies/RTN/new_op_jul/skel/'
 
 
+from scipy import ndimage
+
+def get_edt_evolution():
+    data = np.zeros((128, 128))
+
+    for i in range(99):
+        skel1 = imageio.imread(f'/localhome/asa420/MIAL/data/confocal_movies/RTN/new_op_jul/skel/R6/R6_decon_t0{i:02d}_ch00_skel.png')
+        skel2 = imageio.imread(f'/localhome/asa420/MIAL/data/confocal_movies/RTN/new_op_jul/skel/R6/R6_decon_t0{i+1:02d}_ch00_skel.png')
+
+
+        # get distance transform of skeleton
+        # invert skeleton
+        skel1 = np.invert(skel1)
+        skel2 = np.invert(skel2)
+
+        plt.imshow(skel1)
+        plt.show()
+
+        exit()
+
+        # get distance transform
+        edt1 = ndimage.distance_transform_edt(skel1)
+        edt2 = ndimage.distance_transform_edt(skel2)
+
+        # get the difference
+        # diff = abs(edt2 - edt1)
+        diff = edt2 - edt1
+
+        # # diff = diff + np.abs(np.min(diff))
+        # # plt.hist(diff.flatten())
+
+        # plt.imshow(diff)
+        # plt.show()
+
+        data = data + diff
+
+        # mean_skel = imageio.imread('/localhome/asa420/MIAL/data/confocal_movies/RTN/new_op_jul/er_mean_proc/rtn6_proc_skel.png')
+
+        # # skel = imageio.imread(f'/localhome/asa420/MIAL/data/confocal_movies/RTN/new_op_jul/skel/R6/R6_decon_t0{i:02d}_ch00_skel.png')
+
+        # # skel = np.invert(skel)
+        # mean_skel = np.invert(mean_skel)
+
+        # edt = ndimage.distance_transform_edt(mean_skel)
+
+        # # data = data + edt
+        # plt.imshow(edt)
+        # plt.show()
+
+    # plt.imshow(data)
+    # plt.show()
+
+
+
 def get_mean_img():
     for ser_num in range(1, 32):
         img_list = []
