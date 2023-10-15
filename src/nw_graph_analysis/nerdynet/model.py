7faa3ce1c15c231a8c5e5d8d91af597c858c9b2f
kiryteo <ashwin.samudre@gmail.com>
2023-10-15 00:16:13 -0700

    NerdyNet scripts added

---
+++ src/nw_graph_analysis/nerdynet/model.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+
+class NerdyNet(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(NerdyNet, self).__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(in_channels, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.bottleneck = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(64, out_channels, kernel_size=2, stride=2, output_padding=0),
+        )
+
+    def forward(self, x):
+        x1 = self.encoder(x)
+        x2 = self.bottleneck(x1)
+        x3 = self.decoder(x2)
+        return x3
+
+
---
+++ src/nw_graph_analysis/nerdynet/train.py
@@ -0,0 +1,81 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+
+from model import NerdyNet
+from dataloader import ERDataset
+
+
+in_channels = 1
+out_channels = 1  # Assuming binary segmentation
+model = NerdyNet(in_channels, out_channels)
+
+# Define your loss function and optimizer
+criterion = nn.BCEWithLogitsLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Define your transformation
+transform = transforms.Compose([
+#     transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+])
+
+# Define your dataset
+
+root_dir = '/localhome/asa420/MIAL/data/confocal-data/vess_enh_unet/'
+
+dataset = ERDataset(root_dir, transform=transform)
+
+
+# Assuming an 80-20 train-test split
+train_size = int(0.8 * len(dataset))
+test_size = len(dataset) - train_size
+train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+
+batch_size = 4
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+# Training loop
+num_epochs = 50
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+for epoch in range(num_epochs):
+    model.train()
+    for inputs, masks in train_loader:
+        inputs, masks = inputs.to(device), masks.to(device)
+
+        # Forward pass
+        outputs = model(inputs)
+#         outputs = (torch.sigmoid(outputs) > 0.5).float()
+        loss = criterion(outputs, masks)
+
+        # Backward pass and optimization
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    # Print the training loss for each epoch
+    print(f"Epoch [{epoch + 1}/{num_epochs}], Training Loss: {loss.item()}")
+
+# Testing loop
+model.eval()
+test_loss = 0.0
+with torch.no_grad():
+    for inputs, masks in test_loader:
+        inputs, masks = inputs.to(device), masks.to(device)
+
+        # Forward pass
+        outputs = model(inputs)
+        loss = criterion(outputs, masks)
+        test_loss += loss.item()
+
+# Calculate and print the average test loss
+average_test_loss = test_loss / len(test_loader)
+print(f"Average Test Loss: {average_test_loss}")
+
+# Save the trained model
+torch.save(model.state_dict(), 'unet_model.pth')
