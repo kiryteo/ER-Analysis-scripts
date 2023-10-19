a528ab15f68c2aa3d0c85b8790785b1331196ad3
kiryteo <ashwin.samudre@gmail.com>
2023-10-18 21:34:40 -0700

    Fuzzy area visualization movies creation scripts

--- src/nw_graph_analysis/nerdynet/inference.py
+++ src/nw_graph_analysis/nerdynet/inference.py
@@ -10,11 +10,13 @@ from model import NerdyNet
 from PIL import Image
 
 from skimage import restoration
+from skimage.morphology import erosion, skeletonize
 from skimage.filters import threshold_otsu
 import copy
 import imageio
 import os
 
+from skimage import img_as_uint
 
 home = os.path.expanduser('~')
 
@@ -25,17 +27,63 @@ groups = ['climp', 'control', 'rtn']
 in_channels = 1
 out_channels = 1
 
-model = NerdyNet(in_channels, out_channels)
-model.load_state_dict(torch.load('model.pth'))
 
-for group, i in itertools.product(groups, range(1, 17)):
-    with contextlib.suppress(Exception):
-        imgpath = f'{sted_data_prefix}/{group}/images/sted_{group}{i}_er_mean.png'
+model = NerdyNet(in_channels, out_channels)
+model.load_state_dict(torch.load('/localhome/asa420/unet_oct17.pth'))
+
+# for group, i in itertools.product(groups, range(1, 17)):
+#     with contextlib.suppress(Exception):
+#         imgpath = f'{sted_data_prefix}/{group}/images/sted_{group}{i}_er_mean.png'
+
+#         image = Image.open(imgpath)
+#         transform = transforms.Compose([
+#             transforms.ToTensor(),
+#         ])
+
+#         image = transform(image)
+#         image = image.unsqueeze(0)  # Add batch dimension
+
+#         # Forward pass through the model
+#         model.eval()
+#         with torch.no_grad():
+#             output = model(image.cuda())
+
+#         # Convert the output to probabilities by applying the sigmoid activation
+#         output_probs = torch.sigmoid(output)
+
+#         # Convert tensors to numpy arrays for visualization
+#         image_np = image.squeeze().numpy()
+#         output_probs_np = output_probs.cpu().squeeze().numpy()
+
+#         norm = (output_probs_np - output_probs_np.min()) / (output_probs_np.max() - output_probs_np.min())
+
+#         rb = restoration.rolling_ball(norm)
+#         subt = norm - rb
+
+#         thr = threshold_otsu(subt)
+
+#         subt2 = copy.deepcopy(subt)
+
+#         subt2[subt < thr] = 0.
+#         subt2[subt >= thr] = 255.
+
+#         imageio.imsave(f'{sted_data_prefix}/{group}/nerdynet_v2/sted_{group}{i}_er_mean_pred.png', subt2)
+
+
+def get_temporal_seg():
+    # for seq in range(1, 17):
+        # try:
+    for num in range(1):
+        # imgpath = f'/localhome/asa420/MIAL/data/sted-data/Climp/std/C{seq}_decon_t0{num:02d}_ch00_std.png'
+        # imgpath = f'/localhome/asa420/MIAL/data/sted-data/Control/std/Ct6_decon_t0{num:02d}_ch00_std.png'
+        imgpath = '/localhome/asa420/MIAL/data/sted-data/Control/control6_er_mean.png'
 
         image = Image.open(imgpath)
         transform = transforms.Compose([
             transforms.ToTensor(),
         ])
+
+
 
         image = transform(image)
         image = image.unsqueeze(0)  # Add batch dimension
@@ -43,13 +91,12 @@ for group, i in itertools.product(groups, range(1, 17)):
         # Forward pass through the model
         model.eval()
         with torch.no_grad():
-            output = model(image.cuda())
+            output = model(image)
 
         # Convert the output to probabilities by applying the sigmoid activation
         output_probs = torch.sigmoid(output)
 
         # Convert tensors to numpy arrays for visualization
-        image_np = image.squeeze().numpy()
         output_probs_np = output_probs.cpu().squeeze().numpy()
 
         norm = (output_probs_np - output_probs_np.min()) / (output_probs_np.max() - output_probs_np.min())
@@ -64,4 +111,15 @@ for group, i in itertools.product(groups, range(1, 17)):
         subt2[subt < thr] = 0.
         subt2[subt >= thr] = 255.
 
-        imageio.imsave(f'{sted_data_prefix}/{group}/nerdynet_v2/sted_{group}{i}_er_mean_pred.png', subt2)
+        # imageio.imsave(f'/localhome/asa420/MIAL/data/sted-data/Control/nerdynet_seg/Ct6_decon_t0{num:02d}_ch00_std_seg.png', subt2)
+        imageio.imsave('/localhome/asa420/MIAL/data/sted-data/Control/control6_er_mean_pred_seg.png', subt2)
+
+        # op = erosion(subt2/255.)
+
+        skel = skeletonize(subt2/255.)
+
+        imageio.imsave('/localhome/asa420/MIAL/data/sted-data/Control/control6_er_mean_pred_skel.png', img_as_uint(skel))
+
+        # imageio.imsave(f'/localhome/asa420/MIAL/data/sted-data/Control/nerdynet_skel_no_erosion/Ct6_decon_t0{num:02d}_ch00_std_skel.png', img_as_uint(skel))
+
+# get_temporal_seg()
--- src/nw_graph_analysis/nerdynet/model.py
+++ src/nw_graph_analysis/nerdynet/model.py
@@ -13,7 +13,7 @@ class NerdyNet(nn.Module):
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
 
-        self.bottleneck = nn.Sequential(
+        self.mid_level = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(128, 192, kernel_size=3, stride=1, padding=1),
@@ -30,7 +30,7 @@ class NerdyNet(nn.Module):
 
     def forward(self, x):
         x1 = self.encoder(x)
-        x2 = self.bottleneck(x1)
+        x2 = self.mid_level(x1)
         x3 = self.decoder(x2)
         return x3
 
