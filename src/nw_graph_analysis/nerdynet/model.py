807437a4eae71e7e188bf0310f1b54a41a45ad63
kiryteo <ashwin.samudre@gmail.com>
2023-11-14 20:40:37 -0800

    D4 nERdy and vectoradam added

--- src/nw_graph_analysis/nerdynet/model.py
+++ src/nw_graph_analysis/nerdynet/model.py
@@ -1,9 +1,14 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-class NerdyNet(nn.Module):
+from groupy.gconv.pytorch_gconv.splitgconv2d import P4ConvZ2, P4ConvP4
+from groupy.gconv.pytorch_gconv import P4MConvZ2, P4MConvP4M
+from groupy.gconv.pytorch_gconv.pooling import plane_group_spatial_max_pooling
+
+class nERdy(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super(NerdyNet, self).__init__()
+        super(nERdy, self).__init__()
 
         self.encoder = nn.Sequential(
             nn.Conv2d(in_channels, 64, kernel_size=3, stride=1, padding=1),
@@ -37,3 +42,34 @@ class NerdyNet(nn.Module):
         return x3
 
 
+class D4nERdy(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(D4nERdy, self).__init__()
+        
+
+        self.conv1 = P4MConvZ2(in_channels, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = P4MConvP4M(32, 32, kernel_size=3, stride=1, padding=1)
+
+        self.conv3 = P4MConvP4M(32, 64, kernel_size=3, stride=1, padding=1)
+        self.conv4 = P4MConvP4M(64, 128, kernel_size=3, stride=1, padding=1)
+
+        self.conv5 = P4MConvP4M(128, 64, kernel_size=3, padding=1)
+        self.conv6 = P4MConvP4M(64, 32, kernel_size=3, padding=1)
+
+        self.convt = nn.ConvTranspose2d(32*8, out_channels, kernel_size=2, stride=2, output_padding=0)
+
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = plane_group_spatial_max_pooling(x, 2, 2)
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = F.relu(self.conv5(x))
+        x = F.relu(self.conv6(x))
+        xs = x.size()
+        x = x.view(xs[0], xs[1] * xs[2], xs[3], xs[4])
+#         x = F.avg_pool2d(x, 2, 2)
+        x = self.convt(x)
+
+        return x
